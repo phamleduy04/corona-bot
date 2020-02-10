e494df846e13eae1ad69132ed187935a8e87e92c
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-09 19:02:54 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,36 @@
-const Discord = require('discord.js')
-const ms = require('ms')
-const json_get = require('get-json')
-const {discord_token} = require('./keys.json')
-const url = 'https://uselessfacts.jsph.pl/random.json?language=en'
-
-
+const Discord = require("discord.js");
+const request = require('request')
+const {discord_key, prefix} = require("./config.json")
+const corona_url = "https://en.wikipedia.org/api/rest_v1/page/html/2019%E2%80%9320_Wuhan_coronavirus_outbreak_by_country_and_territory?stash=true";
 const client = new Discord.Client()
-client.login(discord_token);
 
-client.on('ready', () => {
-    console.log(`Bot ${client.user.tag} đã sẵn sàng để hoạt động.`)
+client.login(discord_key);
+
+client.on('ready', () =>{
+    console.log(`Bot ${client.user.username} đã sẵn sàng để hoạt động!`)
 })
 
-client.on('message', (message) => {
-    if (message.content.toLowerCase() === "activate factbot" && message.author.id == "455935236262592512"){
-        message.channel.send(`Bot đã kích hoạt!`)
-        setInterval(async function(){
-            json_get(url,function(error, res){
-                if(error){
-                    return message.channels.send(`Hệ thống lỗi, không lấy được dữ liệu, vui lòng thử lại sau!`)
-                } else {
-                    const fact_channel = client.channel.get('675495264483803168')
-                    const embed = new Discord.RichEmbed()
-                        .setTitle(`Source: ${res.source} `)
-                        .setDescription(res.text)
-                        .setFooter(`By phamleduy04#9999`)
-                    fact_channel.send(embed)
-                }
-            })
-        },ms('12h'))
-    }
-    if (message.content.toLowerCase() === "factbotnow" && message.author.id == "455935236262592512"){
-        json_get(url,function(error, res){
-            if(error){
-                return message.channel.send(`Hệ thống lỗi, không lấy được dữ liệu, vui lòng thử lại sau!`)
-            } else {
-                const fact_channel = client.channels.get('675495264483803168')
-                const embed = new Discord.RichEmbed()
-                    .setTitle(`Source: ${res.source} `)
-                    .setDescription(res.text)
-                    .setFooter(`By phamleduy04#9999`)
-                fact_channel.send(embed)
-            }
-        })
-    }
-    if (message.content.toLowerCase() === "factbothere" && message.author.id == "455935236262592512"){
-        json_get(url,function(error, res){
-            if(error){
-                return message.channel.send(`Hệ thống lỗi, không lấy được dữ liệu, vui lòng thử lại sau!`)
-            } else {
-                const embed = new Discord.RichEmbed()
-                    .setTitle(`Source: ${res.source} `)
-                    .setDescription(res.text)
-                    .setFooter(`By phamleduy04#9999`)
-                message.channel.send(embed)
-            }
+client.on(`message`, async message => {
+    if (message.content.toLowerCase() == `${prefix}corona`){
+        request(corona_url, function(error, response, request){
+            if (error) return message.channel.send(`Bot lỗi, status code: ${response && response.statusCode}`)
+            var begin = request.indexOf('<th align="right">')
+            var end = request.indexOf('<th></th></tr>');
+            var solieu = request.slice(begin,end)
+            var solieu = solieu.replace(/[^a-zA-Z0-9 ]/g, "");
+            var solieu = solieu.replace(/t|h|a|l|i|g|r|n/g, "");
+            var solieu = solieu.split(" ");
+            var xacnhan = solieu.slice(1,2);
+            var die = solieu.slice(2,3);
+            var recoveries= solieu.slice(3,4);
+            const embed = new Discord.RichEmbed()
+                .setAuthor(`Lưu ý: Thông tin cập nhật về bot không phải thời gian thực!`)
+                .setTitle(`Thông tin về virus Corona aka nCoV`)
+                .addField(`Số lượng ca nhiễm: `,`${xacnhan} ca`)
+                .addField(`Số người chết: `,`${die} người`)
+                .addField(`Số người bình phục: `,`${recoveries} người`)
+                .setFooter(`Nguồn: Wikipedia. Made by phamleduy04#9999`)
+            message.channel.send(embed)
         })
     }
 })
