0f3f272a1abce7549d7e73bd66768d5040179d0a
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-17 03:02:38 +0000

    Đổi API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const request = require('request')
 const {discord_key, prefix} = require("./config.json")
-const corona_url = "https://en.wikipedia.org/api/rest_v1/page/html/2019%E2%80%9320_Wuhan_coronavirus_outbreak_by_country_and_territory?stash=true";
+const corona_url = "https://quohat.pythonanywhere.com";
 const client = new Discord.Client()
 
 client.login(discord_key);
@@ -14,16 +14,11 @@ client.on(`message`, async message => {
     if (message.content.toLowerCase() == `${prefix}corona`){
         request(corona_url, function(error, response, request){
             if (error) return message.channel.send(`Bot lỗi, status code: ${response && response.statusCode}`)
-            var begin = request.indexOf('<tr class="sortbottom">')
-            var end = request.indexOf('<tr style="text-align:center;" class="sortbottom">');
-            var solieu = request.slice(begin,end)
-            var solieu = solieu.replace(/[^a-zA-Z0-9 ]/g, "");
-            var solieu = solieu.replace(/c|s|o|t|r|l|a|h|m|b|p|e|i/g, "");
-            var solieu = solieu.trim().split(" ");
-            var xacnhan = solieu.slice(2,3);
-            var die = solieu.slice(3,4);
-            var recoveries= solieu.slice(4,5);
-            const embed = new RichEmbed()
+            var ketqua = request.split(' ')
+            var xacnhan = ketqua[0]
+            var die = ketqua[1]
+            var recoveries= ketqua[2]
+            const embed = new Discord.RichEmbed()
                 .setAuthor(`Lưu ý: Thông tin cập nhật về bot không phải thời gian thực!`)
                 .setTitle(`Thông tin về virus Corona aka nCoV`)
                 .addField(`Số lượng ca nhiễm: `,`${xacnhan} ca`)
